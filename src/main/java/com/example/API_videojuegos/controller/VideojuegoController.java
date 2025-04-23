87b758875137a971447c4ed035d8bd4a478c6259
carlitroxxx <ca.moil@duocuc.cl>
2025-04-23 11:35:40 -0400

    cambios

--- src/main/java/com/example/API_videojuegos/controller/VideojuegoController.java
+++ src/main/java/com/example/API_videojuegos/controller/VideojuegoController.java
@@ -1,6 +1,7 @@
 package com.example.API_videojuegos.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,14 @@ public class VideojuegoController {
     @GetMapping("/top")
     public Videojuego obtenerVideojuegotop(){
         return new Videojuego("The Legend of Zelda: Breath of the Wild","Nintendo Switch",10);
+    }
+    @GetMapping("/hola/{name}")
+    public String saludoDinamico(@PathVariable String name){
+        return "Hola, "+name;
+    }
+    @GetMapping("/suma/{x}/{y}")
+    public String sumaConParametrs(@PathVariable int x, @PathVariable int y){
+        return "La suma de " + x + " + " + y + " es:  "+(x+y);
     }
 
     static class Videojuego{
